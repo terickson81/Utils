0b9c48d7fc22a05e06dba09dacfc98a64f8184cb
Trevor Erickson <terickson81@gmail.com>
2023-06-22 22:49:58 -0400

    Create SsoAuthenticationEntryPoint.java

---
+++ Java/Security/SsoAuthenticationEntryPoint.java
@@ -0,0 +1,17 @@
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class SsoAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response,
+                         AuthenticationException authException) throws IOException, ServletException {
+        // Redirect the user to your SSO provider's login page or SSO initiation URL
+        response.sendRedirect("https://your-sso-provider.com/login");
+    }
+}
